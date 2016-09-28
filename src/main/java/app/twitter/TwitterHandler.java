76a4711eee4c5085cf65d3a46fc4d101f0af4db4
guille <guille10000@gmail.com>
2016-09-28 11:55:25 +0200

    Hashmap for users, using streams to filter?

--- src/main/java/app/twitter/TwitterHandler.java
+++ src/main/java/app/twitter/TwitterHandler.java
@@ -2,8 +2,11 @@ package app.twitter;
 
 import twitter4j.*;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Class that handles all the interaction with the Twitter API
@@ -13,6 +16,11 @@ public class TwitterHandler {
 
     private static int minRetweet = 50;
     private static int minTweets = 10;
+    private static Map<String, Integer> users = new HashMap<>();
+
+    public static Map<String, Integer> getUsers() {
+        return users;
+    }
 
     public static List<Status> getTweets() {
 
@@ -20,11 +28,15 @@ public class TwitterHandler {
           The factory instance is re-useable and thread safe.*/
         Twitter twitter = new TwitterFactory().getInstance();
         List<Status> tweets = new LinkedList<>();
+
         try {
             Query query = new Query("#11s");
             query.setLocale("en");
+            //query.setLang("en");  // nullpointerexception if uncommented ??
             query.resultType(Query.MIXED);
             query.setCount(minTweets);
+
+            // TODO: Twitter async (http://twitter4j.org/en/code-examples.html#asyncAPI)
 
 
             //Filtering
@@ -35,6 +47,12 @@ public class TwitterHandler {
                         tweets.add(tweet);
                     }
                 }
+
+//                List<Status> resultSet = result.getTweets();
+//                resultSet.stream() //parallelStream
+//                        .filter(t -> t.getRetweetCount() >= minRetweet)
+//                        .forEach(t -> tweets.add(t)); // not thread-safe
+
                 query = result.nextQuery();
             } while (tweets.size() < minTweets);
         } catch (TwitterException e) {

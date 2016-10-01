aedb5ae443f486c464e81ca14cf60718743a9b60
Fran Lozano <fjlozanos@gmail.com>
2016-10-01 17:48:45 +0200

    createDatabase script updated

--- src/main/resources/scripts/db/createDatabase.sql
+++ src/main/resources/scripts/db/createDatabase.sql
@@ -1,10 +1,16 @@
 CREATE TABLE tweets
 (
     id BIGINT(20) PRIMARY KEY NOT NULL,
-    text MEDIUMTEXT NOT NULL,
+    user VARCHAR(150) NOT NULL,
     classified TINYINT(1) DEFAULT '0' NOT NULL,
     assertion TINYINT(1),
     topic TINYINT(1),
-    rumor TINYINT(1)
+    rumor TINYINT(1),
+    text MEDIUMTEXT NOT NULL
 );
-
+CREATE INDEX users__index ON tweets (user);
+CREATE TABLE hashtags
+(
+    hashtag VARCHAR(200) PRIMARY KEY NOT NULL,
+    count INT(11)
+);
